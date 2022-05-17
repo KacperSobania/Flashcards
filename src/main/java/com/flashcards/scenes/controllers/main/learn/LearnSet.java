26f3fc83b2dba77bcaa953e82dc222481eb8b882
Kacper Sobania <kacper_s11@wp.pl>
2022-05-17 18:16:37 +0200

    Implemented LearnSet scene

--- src/main/java/com/flashcards/scenes/controllers/main/learn/LearnSet.java
+++ src/main/java/com/flashcards/scenes/controllers/main/learn/LearnSet.java
@@ -11,6 +11,9 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class LearnSet {
 
@@ -27,21 +30,41 @@ public class LearnSet {
 
     DatabaseConnection setsManager = DatabaseConnection.getInstance();
 
+    private final List<Integer> indexesOfCards = drawOrderOfCards();
+    private int indexOfCurrentCard = 1;
+
     public void initialize(){
         titleOfSetLabel.setText(setsManager.getTitleOfSet(setsManager.indexOfChosenSet));
-
+        cardLabel.setText(setsManager.getCardDefinition(setsManager.indexOfChosenSet, indexesOfCards.get(0)));
+        if(setsManager.getNumberOfCards(setsManager.indexOfChosenSet) > 1){
+            nextButton.setVisible(true);
+        }
     }
 
     public void clickCard(){
-
+        if(cardLabel.getText().equals(setsManager.getCardDefinition(setsManager.indexOfChosenSet, indexesOfCards.get(indexOfCurrentCard - 1)))){
+            cardLabel.setText(setsManager.getCardAnswer(setsManager.indexOfChosenSet, indexesOfCards.get(indexOfCurrentCard - 1)));
+        }
+        else {
+            cardLabel.setText(setsManager.getCardDefinition(setsManager.indexOfChosenSet, indexesOfCards.get(indexOfCurrentCard - 1)));
+        }
     }
 
     public void clickNext(){
-
+        previousButton.setVisible(true);
+        indexOfCurrentCard++;
+        cardLabel.setText(setsManager.getCardDefinition(setsManager.indexOfChosenSet, indexesOfCards.get(indexOfCurrentCard-1)));
+        if(indexOfCurrentCard >= indexesOfCards.size())
+            nextButton.setVisible(false);
     }
 
     public void clickPrevious(){
-
+        nextButton.setVisible(true);
+        indexOfCurrentCard--;
+        cardLabel.setText(setsManager.getCardDefinition(setsManager.indexOfChosenSet, indexesOfCards.get(indexOfCurrentCard-1)));
+        if(indexOfCurrentCard <= 1){
+            previousButton.setVisible(false);
+        }
     }
 
     public void clickBack(ActionEvent event) throws IOException {
@@ -50,5 +73,19 @@ public class LearnSet {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
+
+    private List<Integer> drawOrderOfCards() {
+        List<Integer> drawnIndexesOfCards = new ArrayList<>();
+        List<Integer> arrayWithValues = new ArrayList<>();
+        for(int i = 1; i <= setsManager.getNumberOfCards(setsManager.indexOfChosenSet); i++){
+            arrayWithValues.add(i);
+        }
+        for(int i = 0; i < arrayWithValues.size();){
+            int randomNumber = new Random().nextInt(arrayWithValues.size());
+            drawnIndexesOfCards.add(arrayWithValues.get(randomNumber));
+            arrayWithValues.remove(randomNumber);
+        }
+        return drawnIndexesOfCards;
     }
 }
